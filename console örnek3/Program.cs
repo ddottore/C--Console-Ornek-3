408da846d62392b5ffe38164ebdeb478abab1fbc
Emirhan Çalışkan <emrhncalskn@gmail.com>
2022-02-16 22:04:19 +0300

    Sadece Toplama ve Çıkarma kullanılarak Bölme İşlemi

---
+++ console örnek3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Bölünecek sayıyı giriniz. : ");
+int sayi1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Bölen sayıyı giriniz : ");
+int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+for (int i = sayi2; i <= sayi1;)
+{
+    sayi1 = sayi1 - i;
+}
+
+Console.WriteLine(sayi1);
+
+
+//Sadece Toplama ve Çıkarma kullanılarak Bölme İşlemi
